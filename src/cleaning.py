ec136087e8e919259480ca4b22924dcfd5e277b1
HasanBM <68597089+HasanBM@users.noreply.github.com>
2024-10-09 20:32:46 +0100

    Can drop duplicates

--- src/cleaning.py
+++ src/cleaning.py
@@ -7,26 +7,26 @@ file_path = '/Users/HM/Desktop/Code/FOIL_DA_Task-10-2024/data/Online_Retail.xlsx
 
 def load_data(file_path):
 
-    xls_data = pd.read_excel(file_path)
+    df = pd.read_excel(file_path)
 
-    xls_data = xls_data.sort_values(by='InvoiceDate').reset_index(drop=True)
+    df = df.sort_values(by='InvoiceDate').reset_index(drop=True)
 
-    return xls_data.shape, xls_data.head(5), xls_data.info()
+    return df.shape, df.head(5), df.info()
 
 def empty_values(file_path):
     
     
-    xls_data = pd.read_excel(file_path)
+    df = pd.read_excel(file_path)
 
     cols = ['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country']
     
     empty_values_count = {}
     
     for col in cols:
-        if xls_data[col].dtype == 'object':
-            empty_values = xls_data[xls_data[col].isnull() | xls_data[col].str.strip() == ""]
+        if df[col].dtype == 'object':
+            empty_values = df[df[col].isnull() | df[col].str.strip() == ""]
         else:
-            empty_values = xls_data[xls_data[col].isnull()]
+            empty_values = df[df[col].isnull()]
 
         empty_values_count[col] = len(empty_values)
 
@@ -40,16 +40,22 @@ def empty_values(file_path):
 
 def fill_missing_cust_id(file_path):
 
-    xls_data = pd.read_excel(file_path)
+    df = pd.read_excel(file_path)
 
-    prior_missing_cust_ids= xls_data['CustomerID'].isnull().sum()
+    prior_missing_cust_ids= df['CustomerID'].isnull().sum()
 
-    xls_data['CustomerID'] = xls_data['CustomerID'].fillna(-1)
+    df['CustomerID'] = df['CustomerID'].fillna(-1)
 
-    post_missing_cust_ids = xls_data['CustomerID'].isnull().sum()
+    post_missing_cust_ids = df['CustomerID'].isnull().sum()
 
     return prior_missing_cust_ids, post_missing_cust_ids
 
+def drop_duplicates(file_path):
+
+    df = pd.read_excel(file_path)
+    df = df.drop_duplicates()
+    
+    return df
 
 
 
--- test/test_cleaning.py
+++ test/test_cleaning.py
@@ -1,17 +1,45 @@
 import pytest
-from src.cleaning import load_data, fill_missing_cust_id
+import pandas as pd
+import os
+
+from src.cleaning import load_data, fill_missing_cust_id, drop_duplicates
 
 @pytest.mark.it("checks whether the data has the necessary number of columns and is non-empty")
 def test_load_data():
-    file_path = 'data/Online_Retail.xlsx'
+    file_path = '/Users/HM/Desktop/Code/FOIL_DA_Task-10-2024/data/Online_Retail.xlsx'
     data_shape, data_head, data_info = load_data(file_path)
     assert data_shape[1] == 8
     assert data_head.empty == False
 
 @pytest.mark.it("checks whether the missing Customer IDs have been filled")
 def test_missing_IDs():
-    file_path = 'data/Online_Retail.xlsx'
+    file_path = '/Users/HM/Desktop/Code/FOIL_DA_Task-10-2024/data/Online_Retail.xlsx'
     prior_ids, post_ids = fill_missing_cust_id(file_path)
     assert prior_ids > 0
     assert post_ids == 0
 
+@pytest.mark.it("checks whether duplicates have been dropped")
+def test_drop_duplicates():
+    sample_data = {
+        'InvoiceNo': ['536365', '536365', '536366', 'c536367', '536367'],
+        'StockCode': ['71053', '71053', '85123A', '85049', '85049'],
+        'Description': ['WHITE METAL LANTERN', 'WHITE METAL LANTERN', 
+                        'WHITE HANGING HEART T-LIGHT HOLDER', 'RED WOOLLY HOTTIE WHITE HEART.',
+                        'RED WOOLLY HOTTIE WHITE HEART.'],
+        'Quantity': [6, 6, 6, 6, 6],
+        'UnitPrice': [3.39, 3.39, 2.55, 7.65, 7.65],
+        'CustomerID': [17850, 17850, 17850, 17850, 17850],
+        'Country': ['United Kingdom'] * 5
+    }
+
+    df = pd.DataFrame(sample_data)
+    test_file_path = '/Users/HM/Desktop/Code/FOIL_DA_Task-10-2024/data/test_data.xlsx'
+    df.to_excel(test_file_path, index=False)
+
+    clean_data = drop_duplicates(test_file_path)
+    print(clean_data)
+    
+    assert clean_data.shape[0] == 4
+    assert clean_data.duplicated().sum() == 0
+    
+    os.remove(test_file_path)
